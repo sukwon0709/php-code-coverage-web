8f59b9c42b4af2fabb76d113d4a7d383501ebafa
Sukwon Oh <sukwon0709@gmail.com>
2019-09-18 20:37:03 -0400

    names test files by execution time

--- codecoverage/start_xdebug.php
+++ codecoverage/start_xdebug.php
@@ -1,34 +1,34 @@
 <?php
-    $current_dir = __DIR__;
-    $test_name = (isset($_COOKIE['test_name']) && !empty($_COOKIE['test_name'])) ? $_COOKIE['test_name'] : 'unknown_test_' . time();
-    xdebug_start_code_coverage(XDEBUG_CC_UNUSED | XDEBUG_CC_DEAD_CODE);
+$current_dir = __DIR__;
+$curtime = explode(" ", microtime(false));
+$test_name = 'unknown_test_' . date('r', $curtime[1]) . '.' . $curtime[0];
+xdebug_start_code_coverage(XDEBUG_CC_UNUSED | XDEBUG_CC_DEAD_CODE);
 
-    function end_coverage()
+function end_coverage()
+{
+    global $test_name;
+    global $current_dir;
+    $coverageName = $current_dir . '/coverages/coverage-' . $test_name;
+
+    try {
+        xdebug_stop_code_coverage(false);
+        $codecoverageData = json_encode(xdebug_get_code_coverage());
+        file_put_contents($coverageName . '.json', $codecoverageData);
+    } catch (Exception $ex) {
+        file_put_contents($coverageName . '.ex', $ex);
+    }
+}
+
+class coverage_dumper
+{
+    public function __destruct()
     {
-        global $test_name;
-        global $current_dir;
-        $coverageName = $current_dir . '/coverages/coverage-' . $test_name . '-' . microtime(true);
-
         try {
-            xdebug_stop_code_coverage(false);
-            $coverageName = $current_dir . '/coverages/coverage-' . $test_name . '-' . microtime(true);
-            $codecoverageData = json_encode(xdebug_get_code_coverage());
-            file_put_contents($coverageName . '.json', $codecoverageData);
+            end_coverage();
         } catch (Exception $ex) {
-            file_put_contents($coverageName . '.ex', $ex);
+            echo str($ex);
         }
     }
+}
 
-    class coverage_dumper
-    {
-        function __destruct()
-        {
-            try {
-                end_coverage();
-            } catch (Exception $ex) {
-                echo str($ex);
-            }
-        }
-    }
-
-    $_coverage_dumper = new coverage_dumper();
+$_coverage_dumper = new coverage_dumper();
