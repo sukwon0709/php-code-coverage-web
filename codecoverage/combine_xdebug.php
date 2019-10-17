2356225bf6a4a3dc11650d86e8bb63f5bb448b32
Sukwon Oh <sukwon0709@gmail.com>
2019-10-17 15:02:32 -0400

    made coverage tool like a server that watches over new files and updates coverage

--- codecoverage/combine_xdebug.php
+++ codecoverage/combine_xdebug.php
@@ -1,27 +1,41 @@
 <?php
-    include_once("vendor/autoload.php");
-    $coverages = glob("coverages/*.json");
 
-    #increase the memory in multiples of 128M in case of memory error
-    ini_set('memory_limit', '12800M');
+include_once("vendor/autoload.php");
 
-    $final_coverage = new SebastianBergmann\CodeCoverage\CodeCoverage;
-    $count = count($coverages);
-    $i = 0;
+#increase the memory in multiples of 128M in case of memory error
+ini_set('memory_limit', '12800M');
 
-    $final_coverage->filter()->addDirectoryToWhitelist("/home/soh/git/uc-php/dependencies/ucphp_php_apps");
+$seen_coverage_files = array();
 
-    foreach ($coverages as $coverage_file)
-    {
-        $i++;
-        echo "Processing coverage ($i/$count) from $coverage_file". PHP_EOL;
-        $codecoverageData = json_decode(file_get_contents($coverage_file), JSON_OBJECT_AS_ARRAY);
-        $test_name = str_ireplace("coverage-", "", basename($coverage_file,".json"));
-        $final_coverage->append($codecoverageData, $test_name);
-    }
+$final_coverage = new SebastianBergmann\CodeCoverage\CodeCoverage;
+$final_coverage->filter()->addDirectoryToWhitelist("/home/soh/git/uc-php/dependencies/ucphp_php_apps");
 
-    echo "Generating final report..." . PHP_EOL;
-    $report = new \SebastianBergmann\CodeCoverage\Report\Html\Facade;
-    $report->process($final_coverage,"reports");
-    echo "Report generated succesfully". PHP_EOL;
+while (true)
+{
+	$coverages = glob("coverages/*.json");
+	$new_coverage_files = array_diff($coverages, $seen_coverage_files);
+
+	$count = count($new_coverage_files);
+	$i = 0;
+
+	foreach ($new_coverage_files as $coverage_file)
+	{
+		$i++;
+		echo "Processing coverage ($i/$count) from $coverage_file". PHP_EOL;
+		$codecoverageData = json_decode(file_get_contents($coverage_file), JSON_OBJECT_AS_ARRAY);
+		$test_name = str_ireplace("coverage-", "", basename($coverage_file,".json"));
+		$final_coverage->append($codecoverageData, $test_name);
+		array_push($seen_coverage_files, $coverage_file);
+	}
+
+	echo "Generating final report..." . PHP_EOL;
+	$report = new \SebastianBergmann\CodeCoverage\Report\Html\Facade;
+	$report->process($final_coverage,"reports");
+	echo "Report generated succesfully". PHP_EOL;
+
+	echo "\n";
+	sleep(5);
+
+}
+
 ?>
